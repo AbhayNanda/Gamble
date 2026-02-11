37b2bc3b79f09dc8fe06b56d2c6d9a5a0d4564fe
AbhayNanda <abhayne14@gmail.com>
2026-02-11 18:51:56 +0530

    Symbols

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A" : 2,
+    "B" : 3,
+    "C" : 4,
+    "D" : 6
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []                 
+    for symbol, symbol_count in symbol.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+            
+    
+    columns = [[], [] , []]
+    for cols in range(cols):
+        column = []
+        current_symbols = all_symbols[:]                # : is slice operator, without this copy wont work properly
+        for rows in range(rows):
+            value = random.choice(all_symbols)          #Make a Copy of all symbol list to avoid duplication, e.g, we only 2 As and dont need a third A in teh slot machine
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 def deposit():
     while True:
@@ -37,7 +71,7 @@ def get_bet():
                 print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
         else:
             print("Please enter a number.")
-    return amount
+        return amount
 
 def main():
     balance = deposit()    
